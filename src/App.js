f3598fde171c36403018d61e6bb2e091d10c3076
EstherSauri <75174479+EstherSauri@users.noreply.github.com>
2021-03-09 11:14:28 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <Weather />
+      </div>
     </div>
   );
 }
